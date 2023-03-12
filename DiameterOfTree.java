d3e31d16281f08bc5ecc1075d4372c94a09b6a16
Jeevan-Rai <jeevanraisonangeri@gmail.com>
2023-03-12 11:34:05 +0530

    Update DiameterOfTree.java

--- DiameterOfTree.java
+++ DiameterOfTree.java
@@ -29,14 +29,38 @@ public class DiameterOfTree {
         }
     }
 
-    class TreeInfo {
+    static class TreeInfo {
         int ht;
         int diam;
+
+        public TreeInfo(int ht, int diam) {
+            this.ht = ht;
+            this.diam = diam;
+        }
+
+    }
+
+    public static TreeInfo diameter(Node root) {
+        if (root == null) {
+            return new TreeInfo(0, 0);
+        }
+
+        TreeInfo left = diameter(root.lchild);
+        TreeInfo right = diameter(root.rchild);
+
+        int myhight = Math.max(left.ht, right.ht) + 1;
+
+        int diam1 = left.diam;
+        int diam2 = right.diam;
+        int diam3 = left.ht + right.ht + 1;
+        int mydiam = Math.max(Math.max(diam1, diam2), diam3);
+
+        return new TreeInfo(myhight, mydiam);
     }
 
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Node root = BinaryTree.BuildTree(nodes);
-
+        System.out.println(diameter(root).diam);
     }
 }
